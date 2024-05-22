20f7a69dc9b2cd33bfffb1a81d25584c2d65108f
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2024-05-21 20:49:47 -0400

    main menu push

---
+++ Assets/Scripts/UI/UI MenuButtons.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//written by Creature Sari
+public class UIMenuButtons : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
