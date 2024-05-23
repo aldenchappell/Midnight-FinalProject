5161047ddc2fc05df5b915e6e0ea7fa0054eb265
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-05-23 12:26:25 -0400

    Temporary enemy with animations
    Animations are implemented, just need to tweak some values and it will be done.

--- Assets/Scripts/NPC/EnemyStateController.cs
+++ Assets/Scripts/NPC/EnemyStateController.cs
@@ -5,8 +5,8 @@ using UnityEngine.AI;
 public class EnemyStateController : MonoBehaviour
 {
     // Enemy State Parameters
-    enum AIState { roaming, chasing, patrolling };
-    AIState currentState;
+    enum AIState { Roam, Chase, Patrol };
+    private AIState _currentState;
 
     [Header("Movement Speed Values")]
     [SerializeField] private float roamSpeed = 1.85f;
@@ -14,16 +14,23 @@ public class EnemyStateController : MonoBehaviour
     [SerializeField] private float patrollingSpeed = 2.0f;
 
     // Script References
-    EnemyVision COV;
-    RandomMovementController RM;
-    NavMeshAgent agent;
+    private EnemyVision _enemyVision;
+    private RandomMovementController _randomMovement;
+    private NavMeshAgent _agent;
+    private EnemyAnimator _animator;
+    
+    private void Awake()
+    {
+        _enemyVision = GetComponent<EnemyVision>();
+        _randomMovement = GetComponent<RandomMovementController>();
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<EnemyAnimator>();
+    }
 
     private void Start()
     {
-        COV = GetComponent<EnemyVision>();
-        RM = GetComponent<RandomMovementController>();
-        agent = GetComponent<NavMeshAgent>();
-        currentState = AIState.roaming; // Initialize with roaming state
+        _currentState = AIState.Roam; // Initialize with roaming state
+        _animator.SetAnimationTrigger(_currentState.ToString());
     }
 
     private void Update()
@@ -34,38 +41,43 @@ public class EnemyStateController : MonoBehaviour
 
     private void DetermineCurrentState()
     {
-        if (COV.targetsInSight.Count > 0 && COV.realizationValue > 20)
+        if (_enemyVision.targetsInSight.Count > 0 && _enemyVision.realizationValue > 20)
         {
-            currentState = AIState.chasing;
+            _currentState = AIState.Chase;
         }
-        else if (COV.realizationValue > 0)
+        else if (_enemyVision.realizationValue > 0)
         {
-            currentState = AIState.patrolling;
+            _currentState = AIState.Patrol;
         }
         else
         {
-            currentState = AIState.roaming;
+            _currentState = AIState.Roam;
         }
     }
 
     private void ApplyCurrentStateBehaviour()
     {
-        if (currentState == AIState.chasing)
+        
+        if (_currentState == AIState.Chase)
         {
-            Transform target = COV.targetsInSight[0].transform;
-            agent.speed = chaseSpeed;
-            RM.ChasePlayer(target);
+            Transform target = _enemyVision.targetsInSight[0].transform;
+            _agent.speed = chaseSpeed;
+            _randomMovement.ChasePlayer(target);
         }
-        else if (currentState == AIState.patrolling)
+        else if (_currentState == AIState.Patrol)
         {
-            Vector3 lastKnownPosition = COV.lastKnownPosition;
-            agent.speed = patrollingSpeed;
-            RM.Patrol(lastKnownPosition);
+            Vector3 lastKnownPosition = _enemyVision.lastKnownPosition;
+            _agent.speed = patrollingSpeed;
+            _randomMovement.Patrol(lastKnownPosition);
         }
         else
         {
-            agent.speed = roamSpeed;
-            RM.Roam();
+            _agent.speed = roamSpeed;
+            _randomMovement.Roam();
         }
+        
+        //Set animation state based on current state (Animation triggers should be named exactly the same
+        //as the current state's name for this to properly work. - Alden)
+        _animator.SetAnimationTrigger(_currentState.ToString());
     }
 }
