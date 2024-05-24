ed92ced0f2cf332c44c2e6f8fb9e9ce9bd991834
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-05-24 10:53:44 -0400

    Comments for field of view vision

--- Assets/Scripts/NPC/FieldOfView.cs
+++ Assets/Scripts/NPC/FieldOfView.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
+    ///////////////////////////////////////////////////////////////////
+    //Attach this script to an empty gameobject as a child of the enemy. 
+    ///////////////////////////////////////////////////////////////////
     [SerializeField] private Material fieldOfViewMaterial;
     [SerializeField] private float visionRange;
     [SerializeField] private float visionAngle;
@@ -26,18 +29,18 @@ public class FieldOfView : MonoBehaviour
 
     void DrawVisionCone()//this method creates the vision cone mesh
     {
-	int[] triangles = new int[(visionConeResolution - 1) * 3];
+	    int[] triangles = new int[(visionConeResolution - 1) * 3];
     	Vector3[] vertices = new Vector3[visionConeResolution + 1];
         vertices[0] = Vector3.zero;
-        float currentangle = -visionAngle / 2;
+        float currentAngle = -visionAngle / 2;
         float angleIncrement = visionAngle / (visionConeResolution - 1);
         float sine;
         float cosine;
 
         for (int i = 0; i < visionConeResolution; i++)
         {
-            sine = Mathf.Sin(currentangle);
-            cosine = Mathf.Cos(currentangle);
+            sine = Mathf.Sin(currentAngle);
+            cosine = Mathf.Cos(currentAngle);
             Vector3 raycastDirection = (transform.forward * cosine) + (transform.right * sine);
             Vector3 vertForward = (Vector3.forward * cosine) + (Vector3.right * sine);
             if (Physics.Raycast(transform.position, raycastDirection, out RaycastHit hit, visionRange, visionObstructingLayer))
@@ -50,7 +53,7 @@ public class FieldOfView : MonoBehaviour
             }
 
 
-            currentangle += angleIncrement;
+            currentAngle += angleIncrement;
         }
         for (int i = 0, j = 0; i < triangles.Length; i += 3, j++)
         {
