d3e5793ff0564cec78154dbdac767f12a3a3392b
Sitelinear7 <owen0234@gmail.com>
2024-05-25 17:42:24 -0400

    Inventory system basics complete

--- Assets/Scripts/Player/PlayerDualHandInventory.cs
+++ Assets/Scripts/Player/PlayerDualHandInventory.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerDualHandInventory : MonoBehaviour
 {
-    [Header("Storage Location for Inventory")]
-    [SerializeField] Vector3 storageLocation;
+    [Header("Item Hand Location")]
+    [SerializeField] Transform handPosition;
 
     private GameObject[] _inventorySlots;
     private int _currentIndexSelected;
@@ -29,10 +29,20 @@ public class PlayerDualHandInventory : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
             _currentIndexSelected = 0;
+            ShowCurrentIndexItem();
         }
         else if(Input.GetKeyDown(KeyCode.Alpha2))
         {
             _currentIndexSelected = 1;
+            ShowCurrentIndexItem();
+        }
+        foreach(GameObject obj in _inventorySlots)
+        {
+            if(obj != null)
+            {
+                obj.transform.position = handPosition.position;
+                obj.transform.localEulerAngles = handPosition.transform.parent.transform.localEulerAngles;
+            }
         }
     }
 
@@ -42,9 +52,28 @@ public class PlayerDualHandInventory : MonoBehaviour
         {
             _inventorySlots[_currentIndexSelected].SetActive(true);
             _inventorySlots[_currentIndexSelected].transform.position = newObject.transform.position;
+            _inventorySlots[_currentIndexSelected].transform.parent = null;
         }
-        newObject.transform.position = storageLocation;
-        newObject.SetActive(false);
-        _inventorySlots[_currentIndexSelected] = newObject; 
+        newObject.transform.parent = this.gameObject.transform;
+        _inventorySlots[_currentIndexSelected] = newObject;
+        ShowCurrentIndexItem();
     }
+    private void ShowCurrentIndexItem()
+    {
+        foreach(GameObject obj in _inventorySlots)
+        {
+            if(obj != null)
+            {
+                obj.SetActive(false);
+            }
+        }
+        if (_inventorySlots[_currentIndexSelected] != null)
+        {
+            _inventorySlots[_currentIndexSelected].SetActive(true);
+            _inventorySlots[_currentIndexSelected].transform.parent = this.gameObject.transform;
+            _inventorySlots[_currentIndexSelected].transform.localPosition = handPosition.localPosition;
+        }
+    }
+
+    //Notes: Items not swapping visibility when pressing 1 and 2, Items not getting set to correct location when swapping.
 }
