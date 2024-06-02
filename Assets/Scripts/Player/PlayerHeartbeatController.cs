21555428e088c603383c1cf26a1933f0ac1ef827
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-06-02 13:21:02 -0400

    Finished heartbeat system. (Needs new sfx)

--- Assets/Scripts/Player/PlayerHeartbeatController.cs
+++ Assets/Scripts/Player/PlayerHeartbeatController.cs
@@ -9,56 +9,72 @@ public class PlayerHeartbeatController : MonoBehaviour
     [SerializeField] private float volumeChangeSpeed = 1.5f;
     [SerializeField] private float maxDistance = 15.0f;
 
-    private EnemyStateController enemyStateController;
+    private EnemyStateController _enemyStateController;
 
     private void Awake()
     {
         heartbeatAudioSource.volume = 0.0f;
-        heartbeatAudioSource.enabled = false;
+        //heartbeatAudioSource.enabled = false;
 
-        enemyStateController = GameObject.FindGameObjectWithTag("Devil").GetComponent<EnemyStateController>();
+        _enemyStateController = GameObject.FindGameObjectWithTag("Devil").GetComponent<EnemyStateController>();
     }
 
     private void Update()
     {
-        bool isDevilChasing = IsDevilChasing();
+        if (InGameSettingsManager.Instance.enableHeartbeatSounds)
+        {
+            bool isDevilChasing = IsDevilChasing();
 
-        float distanceToEnemy = Vector3.Distance(transform.position, enemyStateController.gameObject.transform.position);
-        bool enemyClose = distanceToEnemy <= maxDistance;
+            float distanceToEnemy = Vector3.Distance(transform.position, _enemyStateController.gameObject.transform.position);
+            bool enemyClose = distanceToEnemy <= maxDistance;
 
-        HandleHeartBeat(isDevilChasing, enemyClose, distanceToEnemy);
+            HandleHeartBeat(isDevilChasing, enemyClose, distanceToEnemy);
+        }
     }
 
-    private bool IsDevilChasing() => enemyStateController.currentState == EnemyStateController.AIState.Chase;
+    private bool IsDevilChasing() => _enemyStateController.currentState == EnemyStateController.AIState.Chase;
 
-    private void HandleHeartBeat(bool isEnemyChasing, bool enemyClose, float distanceToEnemy)
+    private void HandleHeartBeat(bool isEnemyChasing, bool isEnemyClose, float distanceToEnemy)
     {
+        float enemyRealizationValue = _enemyStateController.GetComponent<EnemyVision>().GetRealizationValue;
+
+        //calc the target heartbeat volume based on both realization value and enemy proximity
         float targetHeartbeatVolume = minHeartBeatVolume;
 
-        if (isEnemyChasing && enemyClose)
+        if (isEnemyChasing && isEnemyClose)
         {
+            //calc the proximity factor based on distance
             float enemyProximityFactor = Mathf.Clamp01(1 - (distanceToEnemy / maxDistance));
+
+            //l between min and max heartbeat volume based on proximity factor
             targetHeartbeatVolume = Mathf.Lerp(minHeartBeatVolume, maxHeartBeatVolume, enemyProximityFactor);
-        }
-        
-        //set the volume based on the distance of the player to the enemy 
-        heartbeatAudioSource.volume = Mathf.Lerp(
-            heartbeatAudioSource.volume,
-            targetHeartbeatVolume,
-            Time.deltaTime * volumeChangeSpeed);
-
-        // Enable the audio source if volume is above the minimum volume
-        if (heartbeatAudioSource.volume > minHeartBeatVolume + .01f)
-        {
-            heartbeatAudioSource.enabled = true;
-        }
-        else if (!isEnemyChasing && heartbeatAudioSource.volume <= minHeartBeatVolume + 0.01f)
-        {
-            //disable the audio source after it has reached the minimum volume and the enemy is not chasing
-            heartbeatAudioSource.enabled = false;
-            heartbeatAudioSource.volume = 0.0f;
+            
         }
 
-        Debug.Log(heartbeatAudioSource.volume);
+        //heartbeat volume is based on realization value only when the enemy is not chasing
+        if (!isEnemyChasing)
+        {
+            // Smoothly interpolate the current volume to the minimum volume
+            heartbeatAudioSource.volume = Mathf.Lerp(
+                heartbeatAudioSource.volume,
+                minHeartBeatVolume,
+                Time.deltaTime * volumeChangeSpeed);
+            
+            //set volume to 0 after it has reached the minimum volume and the realization value is decreasing
+            if (heartbeatAudioSource.volume <= minHeartBeatVolume + 0.025f && enemyRealizationValue < 20)
+            {
+                heartbeatAudioSource.volume = 0.0f;
+            }
+        }
+        else
+        {
+            //based on realization value when the enemy is chasing
+            targetHeartbeatVolume *= (enemyRealizationValue / 20f); //should calculate between 0 and 1
+            
+            heartbeatAudioSource.volume = Mathf.Lerp(
+                heartbeatAudioSource.volume,
+                targetHeartbeatVolume,
+                Time.deltaTime * volumeChangeSpeed);
+        }
     }
 }
