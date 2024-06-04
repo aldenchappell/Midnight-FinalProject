ef952c8ce93d9ed4ac1cdda7dfe63ff2975ac40b
aldenchappellWT <aldenchappell03@gmail.com>
2024-06-04 15:13:59 -0400

    Fixed main menu grammar and canvas scaling. Also fixed cursor showing when loading the screen

--- Assets/Scripts/UI/UI MenuButtons.cs
+++ Assets/Scripts/UI/UI MenuButtons.cs
@@ -1,27 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 //written by Creature Sari
 public class UIMenuButtons : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void TestSceneButton()
     {
         SceneManager.LoadScene("TEST SCENE");
+        GlobalCursorManager.Instance.DisableCursor();
     }
 
     public void ExitButton()
