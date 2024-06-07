d28acc7ed4a7a1fadbe27e02179302f6a72e0aa4
Sitelinear7 <owen0234@gmail.com>
2024-06-06 23:28:59 -0400

    Fix to skull dialogue

--- Assets/Scripts/Dialogue/OnlyDialogueNPC.cs
+++ Assets/Scripts/Dialogue/OnlyDialogueNPC.cs
@@ -4,15 +4,52 @@ public class OnlyDialogueNPC : DialogueNPCBase
 {
     public SO_NpcDialogue dialogue;
 
+    private PlayerDualHandInventory _inventory;
+
+    private void Awake()
+    {
+        _inventory = GameObject.FindAnyObjectByType<PlayerDualHandInventory>();
+    }
+
+
     public override void Interact()
     {
-        if (DialogueController.Instance != null)
+        if(IsSkullInInventory())
         {
-            DialogueController.Instance.StartDialogue(dialogue.dialogueLines);
+            if (DialogueController.Instance != null)
+            {
+                DialogueController.Instance.StartDialogue(dialogue.dialogueLines);
+            }
+            else
+            {
+                Debug.LogWarning("DialogueController instance not found!");
+            }
         }
-        else
+    }
+
+    private bool IsSkullInInventory()
+    {
+        /*
+        foreach(GameObject item in _inventory.GetInventory)
         {
-            Debug.LogWarning("DialogueController instance not found!");
+            if(item != null)
+            {
+                if (item.CompareTag("Skull"))
+                {
+                    return false;
+                }
+            }
         }
+        */
+        GameObject[] inventory = _inventory.GetInventory;
+        if(inventory[_inventory.currentIndexSelected] != null)
+        {
+            if (inventory[_inventory.currentIndexSelected].CompareTag("Skull"))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
