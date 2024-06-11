f9b4eb90042c28918996c2e56e480f017161489d
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-06-10 21:20:43 -0400

    enemy footsteps

---
+++ Assets/Scripts/NPC/Devil/EnemyFootsteps.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyFootsteps : MonoBehaviour
+{
+    private EnemyStateController _enemyStateController;
+
+    private AudioSource _source;
+    [SerializeField] private AudioClip[] footstepClips;
+    private float _footstepDelay;
+
+    [SerializeField] private float chaseDelay = .7f;
+    [SerializeField] private float walkDelay = 2.0f;
+
+    private Coroutine _footstepRoutine;
+    
+    private void Awake()
+    {
+        _enemyStateController = GetComponent<EnemyStateController>();
+        _source = GetComponent<AudioSource>();
+        _footstepRoutine = StartCoroutine(Footsteps());
+    }
+
+    private void Update()
+    {
+        _footstepDelay = _enemyStateController.currentState == EnemyStateController.AIState.Chase
+            ? chaseDelay
+            : walkDelay;
+    }
+
+    private IEnumerator Footsteps()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_footstepDelay);
+            AudioClip clip = GetRandomClip(footstepClips);
+            _source.PlayOneShot(clip, 2.0f);
+        }
+    }
+
+    private AudioClip GetRandomClip(AudioClip[] clips)
+    {
+        return footstepClips[Random.Range(0, clips.Length)];
+    }
+}
