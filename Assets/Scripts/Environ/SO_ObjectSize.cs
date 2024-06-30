b7b7c96536ecc478f8effdd9153b69a329b9d862
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-06-30 12:01:10 -0400

    object position for skull companion, fixed a polaroid's position

--- Assets/Scripts/Environ/SO_ObjectSize.cs
+++ Assets/Scripts/Environ/SO_ObjectSize.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Object/ObjectSize", order = 6)]
 public class SO_ObjectSize : ScriptableObject
 {
-    [Tooltip("Depending on the size of the object, this should be 0,1,2. 0 is extra small, 3 is big.")]
-    [Range(0,3)] public int handPositionIndex = 0;
+    [Tooltip("Depending on the size of the object, this should be 0,1,2,3. 0 is extra small, 3 is big.")]
+    [Range(0,4)] public int handPositionIndex = 0;
 }
