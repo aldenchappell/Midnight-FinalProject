a225ad2c8a924a8a91c4c5c32691926ad1e8bf21
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-07-01 18:32:08 -0400

    stuff

--- Assets/Scripts/UI/WinSceneUI.cs
+++ Assets/Scripts/UI/WinSceneUI.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class WinSceneUI : MonoBehaviour
 {
+    [SerializeField] private Button mainMenuButton;
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        
+        mainMenuButton.onClick.AddListener(LevelCompletionManager.Instance.ResetPuzzles);
         
     }
     public void OnMenuButtonPress()
