63b1cc445338ef446af5fa8947019f51b872c6c5
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-07-01 21:13:28 -0400

    ui

--- Assets/Scripts/UI/Settings/BrightnessSlider.cs
+++ Assets/Scripts/UI/Settings/BrightnessSlider.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 public class BrightnessSlider : MonoBehaviour
 {
     private Slider _brightnessSlider;
-    private TMP_Text _valueText;
+    //private TMP_Text _valueText;
     private void Awake()
     {
         _brightnessSlider = GetComponent<Slider>();
-        _valueText = GetComponentInChildren<TMP_Text>();
+        //_valueText = GetComponentInChildren<TMP_Text>();
     }
 
     private void Start()
@@ -18,13 +18,13 @@ public class BrightnessSlider : MonoBehaviour
         float savedBrightness = PlayerPrefs.GetFloat("ScreenBrightness", 0.5f);
         _brightnessSlider.value = savedBrightness;
 
-        _valueText.text = PlayerPrefs.GetFloat("ScreenBrightness").ToString("f1");
+        //_valueText.text = PlayerPrefs.GetFloat("ScreenBrightness").ToString("f1");
     }
 
     public void OnBrightnessSliderValueChanged(float value)
     {
         InGameSettingsManager.Instance.SetBrightness(value);
-        _valueText.text = PlayerPrefs.GetFloat("ScreenBrightness").ToString("f1");
+        //_valueText.text = PlayerPrefs.GetFloat("ScreenBrightness").ToString("f1");
         //Debug.Log(RenderSettings.ambientLight = Color.white * value);
     }
 }
