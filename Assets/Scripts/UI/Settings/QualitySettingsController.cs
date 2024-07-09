775bf1480b86fe2e9acdb60a179e90efb709b04f
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-07-08 22:04:29 -0400

    mm fix

--- Assets/Scripts/UI/Settings/QualitySettingsController.cs
+++ Assets/Scripts/UI/Settings/QualitySettingsController.cs
@@ -6,12 +6,15 @@ public class QualitySettingsController : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown, qualityDropdown;
     private Resolution[] _resolutions;
-    
+
     private void Start()
     {
         _resolutions = Screen.resolutions;
         InitializeResolutionDropdown();
         InitializeQualityDropdown();
+        
+        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", _resolutions.Length - 1);
+        SetResolution(_resolutions.Length - 1);
     }
 
     private void InitializeResolutionDropdown()
@@ -30,27 +33,47 @@ public class QualitySettingsController : MonoBehaviour
             }
         }
         resolutionDropdown.AddOptions(options);
+
+        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", -1);
+        if (savedResolutionIndex == -1 || savedResolutionIndex >= _resolutions.Length)
+        {
+            currentResolutionIndex = _resolutions.Length - 1; // Default to highest resolution
+        }
+        else
+        {
+            currentResolutionIndex = savedResolutionIndex;
+        }
         
-        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
-        resolutionDropdown.value = savedResolutionIndex;
+        Debug.Log("Saved Resolution Index: " + savedResolutionIndex);
+        Debug.Log("Applying Resolution: " + _resolutions[currentResolutionIndex].width + " x " + _resolutions[currentResolutionIndex].height);
+        
+        resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+        Screen.SetResolution(_resolutions[currentResolutionIndex].width, _resolutions[currentResolutionIndex].height, Screen.fullScreen);
     }
 
     private void InitializeQualityDropdown()
     {
         int savedQualityLevel = InGameSettingsManager.Instance.GetQualityLevel();
-        
         qualityDropdown.value = savedQualityLevel;
         qualityDropdown.RefreshShownValue();
     }
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = _resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-        
-        PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
-        PlayerPrefs.Save();
+        if (resolutionIndex >= 0 && resolutionIndex < _resolutions.Length)
+        {
+            Resolution resolution = _resolutions[resolutionIndex];
+            Debug.Log("Setting Resolution: " + resolution.width + " x " + resolution.height);
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+            
+            PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.LogError("Invalid resolution index: " + resolutionIndex);
+        }
     }
 
     public void SetQuality(int qualityLevel)
