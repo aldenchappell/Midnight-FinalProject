acfc60615530f475177c6611decca8e17b885ba8
Sitelinear7 <owen0234@gmail.com>
2024-07-10 22:12:36 -0400

    Some stuff

--- Assets/Scripts/Environ/Lobby/ElevatorObjective.cs
+++ Assets/Scripts/Environ/Lobby/ElevatorObjective.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 public class ElevatorObjective : MonoBehaviour
 {
     private bool shouldCheckTrigger = false;
+
+    private void Awake()
+    {
+        if (LevelCompletionManager.Instance._keysReturned >= 2 && LevelCompletionManager.Instance.hasKey)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
     private void Start()
     {
-        if(LevelCompletionManager.Instance._keysReturned >= 2 && LevelCompletionManager.Instance.hasKey)
-        {
-            return;
-        }
-        else
-        {
-            Invoke(nameof(Disable), 1.0f);
-        }
+
+        Invoke(nameof(Disable), 1.0f);
     }
 
     private void Disable()
