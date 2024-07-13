9ec811b3e2eaaf005d3581b2911d5e71ed2bed82
Alden Chappell <118118263+aldenchappell@users.noreply.github.com>
2024-07-13 13:52:50 -0400

    awd

--- Assets/Scripts/Environ/SO_GroundType.cs
+++ Assets/Scripts/Environ/SO_GroundType.cs
@@ -6,8 +6,7 @@ public class SO_GroundType : ScriptableObject
     //Assign a reference to what sound should be played when the player is on this specific ground type.
     //i.e when the player is on wood, or carpet, etc..
     public AudioClip[] groundTypeAudioClips;
-
-    public float groundTypeVolumeValue; 
     
-    public string groundTypeName;
+    //Volume scale of this ground type. Change this depending on how loud/quiet the footstep sfx are.
+    public float groundTypeVolumeValue;
 }
